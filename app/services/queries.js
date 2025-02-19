d6f5f913868042aab5f675d429420716ad835f8b
Aditya Yadav <dedomil@tuta.io>
2025-02-19 22:07:05 +0530

    add all queries

--- app/services/queries.js
+++ app/services/queries.js
@@ -23,3 +23,47 @@ export function useGetStudentTasksQuery(status, studentId) {
     retry: false,
   });
 }
+
+export function useGetTeacherDashboardQuery(semester, subjectId, division, taskId) {
+  return useQuery({
+    queryKey: ["getTeacherDashboard", semester, subjectId, division, taskId],
+    queryFn: async () => {
+      return (await axios.get(`${API_URL}/teacher/dashboard?semester=${semester}&subjectId=${subjectId}&division=${division}&taskId=${taskId}`)).data;
+    },
+    refetchOnWindowFocus: true,
+    retry: false,
+  });
+}
+
+export function useGetTeacherStudentsListQuery(semester, subjectId, division, taskId) {
+  return useQuery({
+    queryKey: ["getTeacherStudentsList", semester, subjectId, division, taskId],
+    queryFn: async () => {
+      return (await axios.get(`${API_URL}/teacher/students-list?semester=${semester}&subjectId=${subjectId}&division=${division}&taskId=${taskId}`)).data;
+    },
+    refetchOnWindowFocus: true,
+    retry: false,
+  });
+}
+
+export function useGetTeacherReportQuery(teacherSubjectId) {
+  return useQuery({
+    queryKey: ["getTeacherReport", teacherSubjectId],
+    queryFn: async () => {
+      return (await axios.get(`${API_URL}/teacher/generate-report/${teacherSubjectId}`)).data;
+    },
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+}
+
+export function useGetStudentFileQuery(key) {
+  return useQuery({
+    queryKey: ["getStudentFile", key],
+    queryFn: async () => {
+      return (await axios.get(`${API_URL}/student/file/${key}`)).data;
+    },
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+}
